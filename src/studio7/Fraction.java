60e7a3d2d81cd8bc4da9c6734bf6fa5f147c6f82
angelinaviviano <angelinaviviano@angelinas-air.dhcp.wustl.edu>
2023-03-30 15:40:37 -0500

    done

--- src/studio7/Fraction.java
+++ src/studio7/Fraction.java
@@ -1,6 +1,44 @@
 package studio7;
 
-public class Fraction {
+public class Fraction 
+{
+	private int numerator;
+	private int denominator;
+	
+	public Fraction(int n, int d)
+	{
+		this.numerator = n;
+		this.denominator = d;
+	}
+	
+	public Fraction add(Fraction other)
+	{
+		int thisnum = this.numerator*other.denominator;
+		int othernum = this.denominator*other.numerator;
+		int denominator = this.denominator*other.denominator;
+		return (new Fraction(thisnum + othernum, denominator));
+	}
+	
+	public Fraction multiply(Fraction other)
+	{
+		int numerator = this.numerator*other.numerator;
+		int denominator = this.denominator*other.denominator;
+		return (new Fraction(numerator, denominator));
+	}
+	
+	public Fraction reciprocal()
+	{
+		return (new Fraction(this.denominator, this.numerator));
+	}
+	
+	public Fraction simplify()
+	{
+		// did not finish b/c this just math, not learning about classes
+		if (this.numerator % this.denominator == 0)
+		{
+			newNumerator = this.numerator/this.denominator;
+		}
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
--- src/studio7/HockeyPlayer.java
+++ src/studio7/HockeyPlayer.java
@@ -1,8 +1,52 @@
 package studio7;
 
-public class HockeyPlayer {
+public class HockeyPlayer 
+{
+	private String name;
+	private int jerseyNumber;
+	
+	public HockeyPlayer(String n, int jerseyNum, boolean isRightHanded)
+	{
+		this.name = n;
+		this.jerseyNumber = jerseyNum;
+	}
+	
+	public String shootDirection()
+	{
+		double randomNumber = Math.random();
+		if (randomNumber < 0.33)
+		{
+			return "right";
+		}
+		
+		else if (randomNumber < 0.66)
+		{
+			return "left";
+		}
+		
+		else
+		{
+			return "either";
+		}
+	}
+	
+	public String shootHand()
+	{
+		double randomNumber = Math.random();
+		if (randomNumber < 0.5)
+		{
+			return "right";
+		}
+		
+		else
+		{
+			return "left";
+		}
 
-	public static void main(String[] args) {
+	}
+
+	public static void main(String[] args) 
+	{
 		// TODO Auto-generated method stub
 
 	}
